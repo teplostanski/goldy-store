839ac5abf79790a9ea0f7271e9b2d93b85b3dcfa
Irog Teplostanski <teplostanski@yandex.ru>
2023-02-01 22:59:36 +0300

    Добавляет header

---
+++ index.js
@@ -0,0 +1,18 @@
+const selectSingle = document.querySelector('.select');
+const selectSingle_title = selectSingle.querySelector('.select__title');
+const selectSingle_labels = selectSingle.querySelectorAll('.select__label');
+
+selectSingle_title.addEventListener('click', () => {
+  if ('active' === selectSingle.getAttribute('data-state')) {
+    selectSingle.setAttribute('data-state', '');
+  } else {
+    selectSingle.setAttribute('data-state', 'active');
+  }
+});
+
+for (let i = 0; i < selectSingle_labels.length; i++) {
+  selectSingle_labels[i].addEventListener('click', (evt) => {
+    selectSingle_title.textContent = evt.target.textContent;
+    selectSingle.setAttribute('data-state', '');
+  });
+}
